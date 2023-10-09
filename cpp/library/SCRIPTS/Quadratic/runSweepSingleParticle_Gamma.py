8cb962db0cf7d2d6062447425a57c23a05488726
Maksymilian Kliczkowski <klimak97@ui.plgrid.wcss.wroc.pl>
2023-10-09 19:10:31 +0200

    new scripts

--- cpp/library/SCRIPTS/Quadratic/runSweepSingleParticle_Gamma.py
+++ cpp/library/SCRIPTS/Quadratic/runSweepSingleParticle_Gamma.py
@@ -9,15 +9,15 @@ outp    =   open(f"output_SP_{model}.dat", "a+")
 with open(f"input_SP_{model}.ini", "r+") as f:
     lines = f.readlines()
     for l in lines:
-        tmp = l.split("\n")[0]
-        tmp = tmp.split('"')
-        left = tmp[0].split(" ")
-        mid = tmp[1]
-        right = tmp[-1].split(" ")[1:]
-        tmp = left[:-1] + [mid] + right
+        tmp     = l.split("\n")[0]
+        tmp     = tmp.split('"')
+        left    = tmp[0].split(" ")
+        mid     = tmp[1]
+        right   = tmp[-1].split(" ")[1:]
+        tmp     = left[:-1] + [mid] + right
         print(tmp)
-        result = subprocess.run(tmp, stdout=subprocess.PIPE)
-        result = result.stdout.decode('utf-8')
+        result  = subprocess.run(tmp, stdout=subprocess.PIPE)
+        result  = result.stdout.decode('utf-8')
         if '0' not in result:
             print(result)
             break
