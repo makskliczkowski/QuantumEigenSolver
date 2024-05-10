dc16695b0f3a14197012db7a92d0b2ecfd5a83fc
Maks Kliczkowski <kliczkowski@lips.ijs.si>
2024-05-10 14:36:51 +0200

    scripts

---
+++ cpp/library/SCRIPTS/QSM_ETH_Ljubljana2024/srun_qsm_mat_lips.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH -N1
+#SBATCH -c1
+#SBATCH --mem=1gb
+#SBATCH --time=1:00:00
+#SBATCH --job-name=vqmc-loop
+Ns=$1
+PARS=$2
+TIM=$3
+MEM=$4
+CPU=$5
+FUN=$6
+
+RUN_DIR=/home/kliczkowski/CODES/QuantumEigenSolver/cpp/library/
+cd ${RUN_DIR}
+
+savedir=/scratch/kliczkowski/QSM/
+mkdir -p ${savedir}
+
+# create log directory
+SAVDIR=${savedir}/DATA
+mkdir -p $SAVDIR
+LOGDIR=${RUN_DIR}/LOG/QSM
+mkdir -p $LOGDIR
+SLURMDIR=${RUN_DIR}/SLURM
+mkdir -p $SLURMDIR
+
+# parameters
+SSYYMS=$(tr -d ' ' <<< "$PARS")
+
+b="qsm_mat_ns=${Ns},${SSYYMS}"
+arhname="Ns=${Ns},${b},${SLURM_JOBID}.tar.gz"
+a="ns=${Ns}"
+echo "#!/bin/bash" >> ${a}
+echo "#SBATCH -N1" >> ${a}
+echo "#SBATCH --cpus-per-task=${CPU}" >> ${a}
+echo "#SBATCH --mem-per-cpu=${MEM}" >> ${a}
+echo "#SBATCH --time=${TIM}" >> ${a}
+echo "#SBATCH --partition=long" >> ${a}
+echo "#SBATCH --output=${SLURMDIR}/out-%j-${b}.out" >> ${a}
+echo "#SBATCH --job-name=${b}" >> ${a}
+echo "#SBATCH --error=${SLURMDIR}/err_${b}_log-%A-%a.out" >> ${a}
+echo >> ${a}
+echo "module load intel" >> ${a}
+echo >> ${a}
+echo "module load HDF5" >> ${a}
+echo >> ${a}
+echo "cd ${RUN_DIR}" >> ${a}
+echo >> ${a}
+echo "${RUN_DIR}/qsolver.o -qsm_Ntot ${Ns} -qsm_h r\;1.0\;0.5 -fun ${FUN} ${PARS} -th ${CPU} -dir ${SAVDIR}/ >& ${LOGDIR}/log_${b}.log" >> ${a}
+echo >> ${a}
+# echo "tar -cvzf ${SAVDIR}/${arhname} ${SAVDIR}" >> ${a}
+# echo "mv ${SAVDIR}/${arhname} ${lustredir} && rm -rf ${SAVDIR}" >> ${a} 
+
+sbatch ${a} 
+# echo ${a}
+# echo ${b}
+echo $?
+rm ${a}
---
+++ cpp/library/SCRIPTS/skrypt_compile_lips.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -N1
+#SBATCH -c1
+#SBATCH --mem=8gb
+#SBATCH --time=1:00:00
+
+# source /usr/local/sbin/modules.sh
+
+module load intel
+module load HDF5
+
+
+cd ~/CODES/QuantumEigenSolver/cpp/library
+ls
+
+CPPSOURCE=./source/cpp
+ARMADIR=/home/kliczkowski/LIBRARY/armadillo-12.8.0
+
+
+icpx ./main.cpp ./operators.cpp ./user_interface.cpp \
+        ${CPPSOURCE}/common.cpp ${CPPSOURCE}/directories.cpp \
+        ${CPPSOURCE}/exceptions.cpp ${CPPSOURCE}/str.cpp \
+        ${CPPSOURCE}/ui.cpp ${CPPSOURCE}/Lattices/hexagonal.cpp \
+        ${CPPSOURCE}/Lattices/square.cpp -o ./qsolver.o \
+        -I${ARMADIR}/include/  \
+        -pthread -lhdf5 -Wall \
+        -Wformat=0 -lmkl_intel_lp64 -lmkl_core -lmkl_core \
+        -liomp5 -lpthread -lm -ldl -lmkl_sequential \
+        -lstdc++fs -std=c++17 \
+        -fopenmp -std=c++2a -O3 >& compilation.txt
+
+echo "end compile"
