4b937a63312279af40816c3105ae07c9d2e8cb6f
Maks Kliczkowski <maxgrom97@gmail.com>
2024-05-25 23:08:31 +0200

    tie

--- cpp/library/src/user_interface/ui_check_eth.cpp
+++ cpp/library/src/user_interface/ui_check_eth.cpp
@@ -249,7 +249,9 @@ void UI::checkETH_scaling_offdiag(std::shared_ptr<Hamiltonian<_T>> _H)
 	bool isQuadratic		= _H->getIsQuadratic(), 
 		isManyBody			= _H->getIsManyBody();
 
-	const auto [_ops, _opsN]= this->ui_eth_getoperators(isQuadratic, isManyBody);
+	std::vector<Operators::Operator<double>> _ops;
+	strVec _opsN;
+	std::tie(_ops, _opsN)	= this->ui_eth_getoperators(isQuadratic, isManyBody);
 
 	u64 _hs_fractions_diag			= SystemProperties::hs_fraction_diagonal_cut(this->modP.modMidStates_, _Nh);
 
@@ -535,7 +537,9 @@ void UI::checkETH_statistics(std::shared_ptr<Hamiltonian<_T>> _H)
 	bool isQuadratic		= _H->getIsQuadratic(), 
 		 isManyBody			= _H->getIsManyBody();
 
-	const auto [_ops, _opsN]= this->ui_eth_getoperators(_Nh, isQuadratic, isManyBody);
+	std::vector<Operators::Operator<double>> _ops;
+	strVec _opsN;
+	std::tie(_ops, _opsN)	= this->ui_eth_getoperators(isQuadratic, isManyBody);
 
 	// get info
 	std::string modelInfo, dir = "ETH_MAT_STAT", randomStr, extension;
@@ -1476,7 +1480,9 @@ void UI::checkETH_time_evo(std::shared_ptr<Hamiltonian<_T>> _H)
 	bool isQuadratic		= _H->getIsQuadratic(), 
 		isManyBody			= _H->getIsManyBody();
 
-	const auto [_ops, _opsN]= this->ui_eth_getoperators(_Nh, isQuadratic, isManyBody);
+	std::vector<Operators::Operator<double>> _ops;
+	strVec _opsN;
+	std::tie(_ops, _opsN)	= this->ui_eth_getoperators(isQuadratic, isManyBody);
 
 	// get info
 	std::string modelInfo, dir = "ETH_MAT_TIME_EVO", randomStr, extension;
