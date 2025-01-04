d00095b8b57646509129da919722fc7ca6b02256
Maks Kliczkowski <maksymilian.kliczkowski@pwr.edu.pl>
2025-01-04 14:45:36 +0100

    Update SLURM script for ETH simulation: change data directory, enhance logging, and modify qsolver command

--- cpp/library/SCRIPTS/ETH_Ljubljana2024/srun_qsm_mat_makkli.sh
+++ cpp/library/SCRIPTS/ETH_Ljubljana2024/srun_qsm_mat_makkli.sh
@@ -15,13 +15,13 @@ FUN=$6
 RUN_DIR=/home/makkli4548/CODES/QuantumEigenSolver/cpp/library/
 cd ${RUN_DIR}
 
-lustredir=/home/makkli4548/mylustre/DATA/
+lustredir=/home/makkli4548/mylustre/DATA_FIDELITY/
 mkdir -p ${lustredir}
 
 # create log directory
 SAVDIR=${TMPDIR}/DATA/${SLURM_JOBID}
 mkdir -p $SAVDIR
-LOGDIR=${RUN_DIR}/LOG/RANDOM_MODELS
+LOGDIR=${RUN_DIR}/LOG/RANDOM_MODELS_EVO
 mkdir -p $LOGDIR
 
 # parameters
@@ -37,6 +37,7 @@ echo "#SBATCH --mem=${MEM}" >> ${a}
 echo "#SBATCH --time=${TIM}" >> ${a}
 echo "#SBATCH -o ${RUN_DIR}/SLURM/out-%j-${b}.out" >> ${a}
 echo "#SBATCH --job-name=${b}" >> ${a}
+echo "export SLURM_JOB_ID=\$SLURM_JOB_ID" >> ${a}
 echo >> ${a}
 echo "SAVDIR=/lustre/tmp/slurm/\$SLURM_JOB_ID" >> ${a}
 echo >> ${a}
@@ -53,10 +54,11 @@ echo "module load HDF5" >> ${a}
 echo >> ${a}
 echo "cd ${RUN_DIR}" >> ${a}
 echo >> ${a}
-echo "${RUN_DIR}/qsolver.o -Ntot ${Ns} -plrb_mb 1 -Ntots 7 -op 'Sz/L;Sz/0;Sx/0;Sz/0-1;Sz/0-1-2;Sz/0-1-2-3;Sz/1:Lm3:1;Sz/0-1:Lm3:1' -eth_entro 1 -eth_ipr 1 -eth_susc 1 -eth_end '0.1;0.2;0.3;0.4' -eth_offd 1 -fun ${FUN} ${PARS} -th ${CPU} -dir \${SAVDIR}/ >& ${LOGDIR}/log_${b}.log" >> ${a}
+echo "${RUN_DIR}/qsolver -Ntot ${Ns} -plrb_mb 1 -Ntots ${Ns} -op 'Sz/L;Sx/0;Sz/0' -eth_entro 1 -eth_ipr 1 -eth_susc 1 -eth_end '0.1;0.2;0.3;0.4' -eth_offd 1 -checkpoint 0 -fun ${FUN} ${PARS} -th ${CPU} -dir ${lustredir}/ >& ${LOGDIR}/log_${b}.log" >> ${a}
+# echo "${RUN_DIR}/qsolver.o -Ntot ${Ns} -plrb_mb 1 -Ntots 7 -op 'Sz/L;Sz/0;Sx/0;Sz/0-1;Sz/0-1-2;Sz/0-1-2-3;Sz/1:Lm3:1;Sz/0-1:Lm3:1' -eth_entro 1 -eth_ipr 1 -eth_susc 1 -eth_end '0.1;0.2;0.3;0.4' -eth_offd 1 -fun ${FUN} ${PARS} -th ${CPU} -dir \${SAVDIR}/ >& ${LOGDIR}/log_${b}.log" >> ${a}
 echo >> ${a}
-echo "rsync -a --ignore-existing --remove-source-files \${SAVDIR}/* ${lustredir}" >> ${a}
-echo "rm -rf \${SAVDIR}/*" >> ${a} 
+# echo "rsync -a --ignore-existing --remove-source-files \${SAVDIR}/* ${lustredir}" >> ${a}
+# echo "rm -rf \${SAVDIR}/*" >> ${a} 
 echo ${TMPDIR}/DATA/${SLURM_JOBID} >> last_tmp_jobs.log
 sbatch ${a} 
 # echo ${a}
