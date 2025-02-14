37241088c96067a0f1366c65d2e8528fe37065c4
makskliczkowski <maxgrom97@gmail.com>
2025-02-14 02:23:39 +0100

    Remove test notebook and update subproject commit reference; reorganize imports in __init__.py for clarity

--- Python/QES/__init__.py
+++ Python/QES/__init__.py
@@ -1,34 +1,21 @@
-"""
+#QES/__init__.py
 
+"""
 Quantum EigenSolver Package
 @Author: Maksymilian Kliczkowski
 @Email: maksymilian.kliczkowski@pwr.edu.pl
 @Date: 2025-02-01
 """
 
-# Import the lattices and allow for the user to choose the lattice type.
-from .general_python.lattices import Lattice, SquareLattice, HexagonalLattice, choose_lattice
+import sys
+import importlib
 
-# import binary manipulation functions 
-from .general_python.common import __binary__ as BinaryMod
+####################################################################################################
 
-'''
-Import elements related to the algebra of the system like:
-- HilbertSpace : The Hilbert space of the system. It is defined within the algebra module.
-- Hamiltonian  : The Hamiltonian of the system. It is defined within the algebra module.
-'''
-from .Algebra.hilbert import HilbertSpace
+from . import general_python
+from general_python import common
+from general_python import lattices
+from general_python import algebra
+from general_python import maths
 
-from .general_python import algebra
-from .general_python import common
-
-# Import all modules
-
-# Export the modules
-__all__ = [
-    "Lattice",
-    "SquareLattice",
-    "HexagonalLattice",
-    "choose_lattice",
-    "HilbertSpace"
-]
+# -------------------------------------------------------------------------------------------------
