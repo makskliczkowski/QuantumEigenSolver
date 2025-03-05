e04758a0e965bc9a8b42d4cec6efc01acdf57c1b
makskliczkowski <maxgrom97@gmail.com>
2025-03-05 10:39:09 +0100

    Update Sphinx documentation configuration: enhance project information, add templates path, and include Napoleon extension.

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+
 import os
 import sys
+
+# Add the project root directory to sys.path so that Sphinx can find the modules
 sys.path.insert(0, os.path.abspath('..'))
 
-project = 'QuantumEigenSolver'
-author = 'Maksymilian Kliczkowski'
+# -- Project information -----------------------------------------------------
 
+project     = 'Quantum Eigensolver'
+copyright   = '2025, Maksymilian Kliczkowski'
+author      = 'Maksymilian Kliczkowski'
+
+# -- General configuration ---------------------------------------------------
+
+# The master toctree document
+master_doc = 'index'
+
+# Add any Sphinx extension module names here.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
 ]
 
-html_theme = 'sphinx_rtd_theme'
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns to ignore when looking for source files.
+exclude_patterns = ['_build', 'build', 'Thumbs.db', '.DS_Store']
