051eef5f2b13ec67124928deef2c658b03031ccd
Maksymilian Kliczkowski <maksymilian.kliczkowski@pwr.edu.pl>
2025-07-25 17:56:16 +0200

    x

--- Python/projects/2025/um_evolotion/evolution.sh
+++ Python/projects/2025/um_evolotion/evolution.sh
@@ -9,15 +9,24 @@ ls -l
 chmod +x ./evolution.py
 
 # run with:
-# 1) 
-# ../../2025_um_evolution/data_big_times        \
+# 1. save dir
+# 2. alpha_start
+# 3. alpha_step
+# 4. alpha_num
+# 5. number of realizations
+# 6. Ns_start
+# 7. Ns_step
+# 8. additional parameter
+# 9. number of timesteps
+# 10. memory per job
+# 11. maximal memory
 python ./evolution.py      \
     data_big_times          \
     0.7                     \
     0.02                    \
     10                      \
-    50                     \
-    13                      \
+    10                     \
+    14                      \
     14                      \
     1                       \
     100000                  \
