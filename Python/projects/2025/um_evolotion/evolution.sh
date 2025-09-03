2798f3d30af708454056d106d83a870f66c1b6ae
makskliczkowski <maxgrom97@gmail.com>
2025-09-04 01:18:35 +0200

    Implement code changes to enhance functionality and improve performance

--- Python/projects/2025/um_evolotion/evolution.sh
+++ Python/projects/2025/um_evolotion/evolution.sh
@@ -20,11 +20,11 @@ chmod +x ./evolution.py
 python ./evolution.py \
     --save_dir data_big_times_fixed_f \
     --alpha_start 0.7 \
-    --alpha_step 0.04 \
-    --alphas_number 7 \
+    --alpha_step 0.1 \
+    --alphas_number 3 \
     --sites_start 8 \
     --sites_end 11 \
-    --number_of_realizations 100 \
+    --number_of_realizations 10 \
     --n 1 \
     --time_num 100000 \
     --memory_per_worker 64 \
