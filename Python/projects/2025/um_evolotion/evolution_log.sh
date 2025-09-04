c9b8ef17d8f5a794f9d04f31e580b0c40c1263e7
makskliczkowski <maxgrom97@gmail.com>
2025-09-04 19:57:06 +0200

    evo_log

---
+++ Python/projects/2025/um_evolotion/evolution_log.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+cd "$(dirname "$0")"
+# make sure script is executable
+echo "Running evolution script in $(pwd)"
+chmod +x ./evolution.py
+
+# run with:
+# 1. save dir
+# 2. alpha_start
+# 3. alpha_step
+# 4. alpha_num
+# 5. number of realizations
+# 6. Ns_start
+# 7. Ns_stop
+# 8. additional parameter
+# 9. number of timesteps
+# 10. memory per job
+# 11. maximal memory
+    # --save_dir data_big_times_fixed_f \
+python ./evolution.py \
+    --save_dir data_log \
+    --alpha_start 0.52 \
+    --alpha_step 0.04 \
+    --alphas_number 5 \
+    --sites_start 10 \
+    --sites_end 13 \
+    --number_of_realizations 5 \
+    --n 1 \
+    --time_num 10000 \
+    --memory_per_worker 64 \
+    --max_memory 64 \
+    --model um \
+    --uniform 0
