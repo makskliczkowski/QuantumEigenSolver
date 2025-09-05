f71c336c41d20d2a798a1107dde24d1ac7f3b58e
makskliczkowski <maxgrom97@gmail.com>
2025-09-05 19:45:11 +0200

    x

--- Python/projects/2025/um_evolotion/scripts/evolution.sh
+++ Python/projects/2025/um_evolotion/scripts/evolution.sh
@@ -227,7 +227,7 @@ main() {
         echo "echo -e \"\$(pwd)\t\$(date)\" >> ${defaults[RUN_DIR]}/slurm_ids_run.log"
         echo ""
 
-        add_module_section "Python/3.10.8-GCCcore-12.2.0"
+        add_module_section "Python/3.11.3-GCCcore-12.3.0"
         echo "venv_name=\"${venv_name}\""
         echo "venv_path=\"${venv_path}\""
         setup_qes_environment "${defaults[QES_PACKAGE_DIR]}" "requirements/requirements.txt" "$venv_name" "$venv_path" || exit 1
